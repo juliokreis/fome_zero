538734c5a87c3eb5484f111e2517b67ba6db2470
Julio Reis <55958436+juliokreis@users.noreply.github.com>
2024-11-02 18:43:31 -0300

    Update 2_paises.py

--- pages/2_paises.py
+++ pages/2_paises.py
@@ -70,7 +70,7 @@ with st.container():
     st.markdown('## VISÃO PAÍSES')
 
     # Filtro multiseletor de paises
-    country_options = st.sidebar.multiselect('', sorted(set(df['country_code'].unique())),
+    country_options = st.sidebar.multiselect('', sorted(set(df['country_name'].unique())),
         default=['Brazil','India','United States of America','South Africa', 'Canada'] )
         
     linhas_selecionadas = df['country_code'].isin(country_options)
