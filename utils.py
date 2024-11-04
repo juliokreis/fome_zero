c37d2f086a1bd45feb7f9125a98233ffa9954a25
Julio Reis <55958436+juliokreis@users.noreply.github.com>
2024-11-04 13:57:36 -0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -81,25 +81,48 @@ def rename_columns(df_raw):
 
 
 # 4.Função que gera o código ao nome de cada pais
+
+COUNTRIES = {
+    1: "India",
+    14: "Australia",
+    30: "Brazil",
+    37: "Canada",
+    94: "Indonesia",
+    148: "New Zeland",
+    162: "Philippines",
+    166: "Qatar",
+    184: "Singapure",
+    189: "South Africa",
+    191: "Sri Lanka",
+    208: "Turkey",
+    214: "United Arab Emirates",
+    215: "England",
+    216: "United States of America",
+}
+
 def country_name(country_code):
-    COUNTRIES = {
-        1: 'India',
-        14: 'Australia',
-        30: 'Brazil',
-        37: 'Canada',
-        94: 'Indonesia',
-        148: 'New Zeland',
-        162: 'Philippines',
-        166: 'Qatar',
-        184: 'Singapure',
-        189: 'South Africa',
-        191: 'Sri Lanka',
-        208: 'Turkey',
-        214: 'United Arab Emirates',
-        215: 'England',
-        216: 'United States of America'
-    }
-    return COUNTRIES[country_code]
+return COUNTRIES[country_code]
+
+
+# def country_name(country_code):
+#     COUNTRIES = {
+#         1: 'India',
+#         14: 'Australia',
+#         30: 'Brazil',
+#         37: 'Canada',
+#         94: 'Indonesia',
+#         148: 'New Zeland',
+#         162: 'Philippines',
+#         166: 'Qatar',
+#         184: 'Singapure',
+#         189: 'South Africa',
+#         191: 'Sri Lanka',
+#         208: 'Turkey',
+#         214: 'United Arab Emirates',
+#         215: 'England',
+#         216: 'United States of America'
+#     }
+#     return COUNTRIES[country_code]
 
 
 
