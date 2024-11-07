66cd8c6a38696a98a86554396f718f66944aed61
Julio Reis <55958436+juliokreis@users.noreply.github.com>
2024-11-07 11:37:21 -0300

    Update 2_paises.py

--- pages/2_paises.py
+++ pages/2_paises.py
@@ -15,14 +15,24 @@ from streamlit_folium import folium_static
 # ----------------------------------------------------------------
 # Chama funções em util.py
 
-# 2. df_raw recebe função 'def extract_data' em utils
+# df_raw recebe função 'def extract_data' em utils
 df_raw = us.extract_data()
 
-# 3. Copia o dataframe original (df_raw) para o de trabalho (df)
+# Copia o dataframe original (df_raw) para o de trabalho (df)
 df = df_raw.copy()
 
-# 4.Função que renomea a colunas
+# Função que renomea a colunas
 df = us.rename_columns(df)
+
+# Função que renomea a colunas
+df = us.rename_columns(df)
+
+# Função que gera o código ao nome de cada pais
+df['country_code'] = df['country_code'].apply(us.country_name)
+
+# Função que categoriza a comida peloa faixa de preço
+df['price_type'] = df.loc[:, 'price_range'].apply(lambda x: us.create_price_tye(x))
+
 
 # # caminho relativo (deploy)
 # def extract_data(path='data/zomato.csv'):
@@ -44,48 +54,49 @@ df = us.rename_columns(df)
 # 4.Função que gera o código ao nome de cada pais
 df['country_name'] = df['country_code'].apply(us.country_name)
 
-# GRÁFICAS
+
+# ------------------------------------------------------------------------------------------
+# FUNÇÕES GRÁFICAS
 
 # 1. Qual o nome do país que possui mais cidades registradas?
-def cidades_por_pais(df):
-
-    # Cria uma nova coluna 'country_name' substituindo o código pelo nome
-    df['country_name'] = df['country_code'].apply(us.country_name)
-    
+def city_country(df):
     # Agrupa a quantidade de cidades por país usando o nome do país
-    pais = df.groupby('country_name')['city'].nunique().sort_values(ascending=False).reset_index()
-    
+    pais = df.groupby('country_code')['city'].nunique().sort_values(ascending=False).reset_index()
+
     # gráfico
-    fig = px.bar(pais, x=pais['country_name'], y=pais['city'],
-                title='Quantidade de cidades por País',
-                labels={'country_code': 'País', 'city': 'Qtde Cidades'})
+    fig = px.bar(pais, x=pais['country_code'], y=pais['city']
+                 ,title='Quantidade de cidades por País'
+                 ,labels={'country_code': 'País', 'city': 'Qtde Cidades'})
     fig.update_traces(texttemplate = '%{y}') 
     return fig
     
 # 2. Qual o nome do país que possui mais restaurantes registrados?
-def restaurantes_por_pais(df):
+def restaurant_country(df):
     # Agrupa a quantidade de cidades por país usando o nome do país
-    pais = df.groupby('country_code')['restaurant_id'].nunique().sort_values(ascending=False).reset_index()
+    country=(df.groupby('country_code')['restaurant_id']
+            .nunique()
+            .sort_values(ascending=False)
+            .reset_index())
 
     # gráfico
-    fig = px.bar(pais, x='country_code', y='restaurant_id')
-    fig = px.bar(pais, x='country_code',y='restaurant_id',
-                title='Quantidade de cidades por País',
-                labels={'country_code': 'País', 'restaurant_id': 'Qtde de restaurantes'})
+    fig = px.bar(country, x=country['country_code'], y=country['restaurant_id']
+                 ,title='Quantidade de cidades por País'
+                 ,labels={'country_code': 'País', 'restaurant_id': 'Qtde de restaurantes'})
     fig.update_traces(texttemplate = '%{y}')
     return fig
 
-def nivel_4(df):
-    df['price_type'] = df.loc[:, 'price_range'].apply(lambda x: create_price_tye(x))
 
-    price_type_counts = df['price_type'].value_counts().reset_index()
-    price_type_counts.columns = ['price_type', 'count']
-    df.sort_values(by='count', ascending=False)
-    
-    # grafico de barras
-    fig = px.bar(df, x='country_name',y='restaurant_id',
-                title='Países com restaurantes nível 4',
-                labels={'country_name': 'País', 'restaurant_id': 'Qtde de restaurantes'})
+# 3. Qual o nome do país que possui mais restaurantes com o nível de preço igual a 4 registrados?
+def level_price(df): 
+    level_price=(df.loc[df['price_range'] == 4, ['restaurant_id', 'country_code']]
+                 .groupby('country_code')
+                 .count()
+                 .sort_values('restaurant_id', ascending=False)
+                 .reset_index())
+    # gráfico
+    fig = px.bar(level_price, x=level_price['country_code'],y=level_price['restaurant_id']
+                 ,title='Países com restaurantes nível 4'
+                 ,labels={'country_code': 'País', 'restaurant_id': 'Qtde restaurant gourmet'})
     fig.update_traces(texttemplate = '%{y}')
     return fig
 
@@ -99,10 +110,10 @@ st.sidebar.image(image, use_column_width='auto')
 st.sidebar.markdown('### Escolha o país.')
 st.sidebar.markdown('''---''')
 
-with st.container():
+with st.sidebar.container():
     st.markdown('## VISÃO PAÍSES')
 
-    country_options = st.sidebar.multiselect('', sorted(set(df['country_name'].unique())),
+    country_options = st.sidebar.multiselect('', sorted(set(df['country_code'].unique())),
     default=['Brazil', 'India','United States of America','South Africa', 'Canada'] )
     
     linhas_selecionadas = df['country_name'].isin(country_options)
@@ -115,23 +126,21 @@ st.sidebar.markdown('''## Powered by Júlio Reis''')
 # LAYOUT STREAMLIT
 
 with st.container():
-    # Chama a função 'def cidades_por_pais' para plotar o gráfico de barras
-    fig = cidades_por_pais(df)
+    # Chama a função 'def city_country' para plotar o gráfico de barras
+    fig = city_country(df)
     st.plotly_chart(fig, use_container_width=True)
 
 with st.container():
     col1, col2 = st.columns(2)
     with col1:
-        st.markdown('Gráfico 2')
-        # Chama a função 'def cidades_por_pais' para plotar o gráfico de barras
-        # fig = restaurantes_por_pais(df)
-        # st.plotly_chart(fig, use_container_width=True)
+        # Chama a função 'def restaurant_country' para plotar o gráfico de barras
+        fig = restaurant_country(df)
+        st.plotly_chart(fig, use_container_width=True)
 
     with col2:
-        st.markdown('Gráfico 3')
-        # Chama a função 'def cidades_por_pais' para plotar o gráfico de barras
-        # fig = nivel_4(df)
-        # st.plotly_chart(fig, use_container_width=True)
+        # Chama a função 'def level_price' para plotar o gráfico de barras
+        fig = level_price(df)
+        st.plotly_chart(fig, use_container_width=True)
 
         
         
